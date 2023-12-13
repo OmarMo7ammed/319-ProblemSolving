dc4f7fdf8108296a4b6ddb1608ed8b08c1290551
OmarMo7ammed <om926933@gmail.com>
2023-12-13 16:39:10 +0200

    Continue Problems

--- Problems.php
+++ Problems.php
@@ -63,14 +63,132 @@
     $sentence = "EraaSoft Learn by practice";
     $position = strpos($sentence,"EraaSoft");
     echo "The Word : ". substr($sentence, $position, strlen("EraaSoft")) . '<br>';
-    
-    // if ($position !== false) {
-    //     $foundWord = substr($sentence, $position, strlen('EraaSoft'));
-    //     echo "Found the word: $foundWord";
-    // } else {
-    //     echo "Word not found";
-    // }
     ///////////////////////////////////////////////////
+    // 11- Remove the word (by) from the string and print the string with and without (by)
+    $sentence = "EraaSoft Learn by practice";
+    $length = strlen("by");
+    $pos = strpos($sentence,"by");
+    $without = str_replace("by"," ",$sentence);
+    echo "With by : " . $sentence . '<br>';
+    echo "Without by : " . $without . '<br>';
+    /////////////////////////////////////////////////
+    // 12- Make a new variable called (Full_string) that concatenate string_one and string_two
+    $string_one = "Eraa";
+    $string_two = "Soft";
+    $full_string = $string_one.$string_two;
+    echo $full_string . '<br>';
+    /////////////////////////////////////////////////
+    // 13- Compare the full_string and this string (EraaSoft).
+    if ($full_string === "EraaSoft"){
+        echo "The full string matches 'EraaSoft' <br>";
+    }else {
+        echo "The full string does not matche 'EraaSoft' <br>";
+    }
+    ///////////////////////////////////////////////////
+    // 14- Write a PHP script to split the following string.
+    // Sample string: 'ErraSoft' 
+    // Expected Output: Er/ra/So/ft
+    $string = 'ErraSoft';
+    $characters = str_split($string,2);
+    $split_string = implode("/",$characters);
+    echo "Sample String : 'ErraSoft' <br>";
+    echo "Expected Output : $split_string <br>";
+    //////////////////////////////////////////////////
+    // 15- Write a PHP script that stores the number as a variable and checks if it is odd or even.
+    $num = 12;
+    if($num % 2 == 0){
+        echo "$num is Even Number <br>";
+    }else{
+        echo "$num is  Odd Number <br>";
+    }
+    //////////////////////////////////////////////////
+    // 16- Write a PHP script that stores the string as a variable and checks if the length is odd or even.
+    $str = "Omar Mohammed";
+    if(strlen($str) % 2 == 0){
+        echo "$str is Even Number <br>";
+    }else {
+        echo "$str is Odd Number <br>";
+    }
+    //////////////////////////////////////////////////
+    // 17- Check from this string o If the string has “gain” Print ( success word ) o
+    //  If the string has ( peen ) Print ( success word )  Else ( wrong word )
+    $description = "no pain , no gain ";
+    if (str_contains($description,"gain") || str_contains($description,"peen")){
+        echo "Success Word <br>";
+    }else {
+        echo "Wrong Word <br>";
+    }
+    //////////////////////////////////////////////////
+    // 18- A Boolean is a data type that has only two values true or false. 
+    // These values often correspond to 1 (true) or 0 (false). When a 1 or a 0 is used,
+    //  it's called an int Boolean. Write a PHP script that stores an int Boolean and outputs 
+    //  its opposite (1 becomes 0 and 0 becomes 1).
+    $booleanValue = 0;
+    if ($booleanValue == 1){
+        echo "Original Value : $booleanValue <br>";
+    }elseif($oppositeValue = !$booleanValue){
+        echo "Opposite Value : $oppositeValue <br>";
+    }
+    ////////////////////////////////////////////////////
+    // 19- Write a PHP script that stores a word and determines Is the Word is 
+    // Singular or Plural? (A plural word is one that ends in "s".)
+    $wordd = "Omar";
+    if(substr($wordd,-1) === 's'){
+        echo "$worrd is Singular Value <br>";
+    }else{
+        echo "$wordd is Plural Value <br>";
+    }
+    /////////////////////////////////////////////////////
+    // 20- Make a calculator with these operations using if and else if
+    // o Submission 
+    // o Subtraction 
+    // o Multiplication 
+    // o Division 
+    // o Power 
+    // o Modulus
+    $num1 = 4;
+    $num2 = 2;
+    function submission($num1,$num2){
+        return $num1 + $num2;  // 6
+    }
+    function subtraction($num1,$num2){
+        return $num1 - $num2;  // 2
+    }
+    function multiplication($num1,$num2){
+        return $num1 * $num2;  // 8
+    }
+    function division($num1,$num2){
+        if($num2 != 0){
+            return $num1 / $num2; // 2
+        }else{
+            return "Cannot Divide by Zero";
+        }
+    }
+    function power($num1,$num2){
+        return pow($num1,$num2);  // 16
+    }
+    function modulus($num1,$num2){
+        if($num2 != 0){
+            return $num1 % $num2;
+        }else{
+            return "Cannot find modulus with zero";
+        }
+    }
+
+    echo "Addition: ".submission($num1,$num2).'<br>';
+    echo "Subtraction: ".subtraction($num1,$num2).'<br>';
+    echo "Multiplication: ".multiplication($num1,$num2).'<br>';
+    echo "Division: ".division($num1,$num2).'<br>';
+    echo "Power: ".power($num1,$num2).'<br>';
+    echo "Modulus: ".modulus($num1,$num2).'<br>';
+
+ 
+
+    
+    
+    
+
+
 
 
 
